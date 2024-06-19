deaadffaf7808bb1beb828611a299bf39ccbf86a
fershad <27988517+fershad@users.noreply.github.com>
2024-06-19 13:37:51 +0800

    import from esm.sh

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { co2 } from '@tgwf/co2';
+import { co2 } from 'https://esm.sh/@tgwf/co2@latest';
 
 function getPageWeight() {
     const performance = window.performance.getEntriesByType('resource');
