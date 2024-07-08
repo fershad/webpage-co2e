0152c88a980d288250c4fdd0b30d20691aef2976
fershad <27988517+fershad@users.noreply.github.com>
2024-07-08 20:55:53 +0100

    link to node_modules

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { co2 } from '@tgwf/co2';
+import { co2 } from './node_modules/@tgwf/co2/dist/esm/index.js';
 
 function getPageWeight() {
     const performance = window.performance.getEntriesByType('resource');
